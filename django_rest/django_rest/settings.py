388096d4fe5322e599eeeb0fb453a024fa8f2e68
fantaso <fantaso.code@gmail.com>
2019-04-19 23:58:52 +0200

    updated restfw test_request_format to JSON

--- django_rest/django_rest/settings.py
+++ django_rest/django_rest/settings.py
@@ -115,5 +115,8 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
-    ]
+    ],
+
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+
 }
