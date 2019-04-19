6ea85d69c76f09895714d34ac05c3e458ca4991a
fantaso <fantaso.code@gmail.com>
2019-04-20 00:01:42 +0200

    added first 4 test for each model in todo api

--- django_rest/todo_api/tests.py
+++ django_rest/todo_api/tests.py
@@ -1,66 +1,109 @@
-from django.contrib.auth import get_user_model
-from rest_framework_jwt.settings import api_settings
-from datetime import datetime, timedelta
+# from django.contrib.auth import get_user_model
+# from rest_framework_jwt.settings import api_settings
 
-from django.test import TestCase
+
+from django.urls import reverse
+
 from rest_framework import status
-from rest_framework.reverse import reverse as api_reverse
+# from rest_framework.reverse import reverse as api_reverse
 from rest_framework.test import APITestCase
 
 from .models import Comment, Project, Reminder, Task
 
+from django.utils import timezone
+from datetime import datetime, timedelta
+import pytz
 
-class ProjectAPITestCase(APITestCase):
+
+######### TESTS PROJECT #########
+class ProjectTests(APITestCase):
+    def test_create_project(self):
+        """Ensure we can create a project."""
+
+        url = reverse('project-list')
+        data = {'name': 'Python Project'}
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Project.objects.count(), 1)
+        self.assertEqual(Project.objects.get().name, 'Python Project')
+
+
+######### TESTS TASK #########
+class TaskTests(APITestCase):
     def setUp(self):
-        project = Project.objects.create(name='Django Rest Framework')
-        task_one = Task.objects.create(project=project,
-                                       title='Define App Features',
-                                       description='apps, models, permissions, etc',
-                                       deadline=datetime.today() + timedelta(days=1),
-                                       priority=Task.LOW)
+        project = Project.objects.create(name='Python Project')
 
-        task_two = Task.objects.create(project=project,
-                                       title='Define App API',
-                                       description='all URIs, serializers, views, etc',
-                                       deadline=datetime.today() + timedelta(days=5),
-                                       priority=Task.LOW)
+    def test_create_task(self):
+        """Ensure we can create a Task."""
 
-        task_three = Task.objects.create(project=project,
-                                         title='Define App Tests',
-                                         description='create,retrieve, delete, requests, validations, etc',
-                                         deadline=datetime.today() + timedelta(days=10),
-                                         priority=Task.LOW)
+        project = Project.objects.filter(name='Python Project').first()
 
-        comment_one_task_one = Comment.objects.create(task=task_one,
-                                                      comment='Donot forget to register the models in the admin site')
-        comment_two_task_one = Comment.objects.create(task=task_one,
-                                                      comment='It is done!')
-        comment_one_task_two = Comment.objects.create(task=task_two,
-                                                      comment='Add a custom queryset')
-        comment_one_task_three = Comment.objects.create(task=task_three,
-                                                        comment='Test deadline validation')
+        url = reverse('task-list')
+        data = {'project': project.id,
+                'title': 'Define App API',
+                'description': 'all URIs, serializers, views, etc',
+                'deadline': (timezone.now() + timedelta(days=5)),
+                'priority': Task.LOW}
 
-        reminder_one_task_one = Reminder.objects.create(task=task_one,
-                                                        date=(datetime.today() + timedelta(hours=12)))
-        reminder_two_task_one = Reminder.objects.create(task=task_one,
-                                                        date=(datetime.today() + timedelta(hours=18)))
-        reminder_one_task_two = Reminder.objects.create(task=task_two,
-                                                        date=(datetime.today() + timedelta(days=2)))
+        response = self.client.post(url, data)
 
-    def test_single_project(self):
-        project_count = Project.objects.count()
-        self.assertEqual(project_count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(Task.objects.get().title, 'Define App API')
+        self.assertEqual(Task.objects.get().priority, 1)
 
-    def test_triple_tasks(self):
-        tasks_count = Task.objects.count()
-        self.assertEqual(tasks_count, 3)
 
-    def test_triple_tasks(self):
-        tasks_count = Task.objects.count()
-        self.assertEqual(tasks_count, 3)
+######### TESTS COMMENT #########
+class CommentTests(APITestCase):
+    def setUp(self):
+        project = Project.objects.create(name='Python Project')
+        task = Task.objects.create(project=project,
+                                   title='Define App Features',
+                                   description='apps, models, permissions, etc',
+                                   deadline=(timezone.now() + timedelta(days=1)),
+                                   priority=Task.LOW,)
 
-    def test_project_list(self):
-        data = {}
-        url = api_reverse('todo:projects')
-        response = self.client.get(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_create_comment(self):
+        """Ensure we can create a Comment."""
+
+        task = Task.objects.filter(title='Define App Features').first()
+
+        url = reverse('comment-list')
+        data = {'task': task.id,
+                'comment': 'Donot forget to register the models in the admin site'}
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Comment.objects.count(), 1)
+        self.assertEqual(Comment.objects.get().comment,
+                         'Donot forget to register the models in the admin site')
+
+
+######### TESTS REMINDER #########
+class ReminderTests(APITestCase):
+    def setUp(self):
+        project = Project.objects.create(name='Python Project')
+        task = Task.objects.create(project=project,
+                                   title='Define App Features',
+                                   description='apps, models, permissions, etc',
+                                   deadline=(timezone.now() + timedelta(days=1)),
+                                   priority=Task.LOW,)
+
+    def test_create_reminder(self):
+        """Ensure we can create a Reminder."""
+
+        task = Task.objects.filter(title='Define App Features').first()
+
+        url = reverse('reminder-list')
+        data = {'task': task.id,
+                'date': datetime(2019, 4, 20, 21, 0, 0, tzinfo=pytz.UTC)}
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Reminder.objects.count(), 1)
+        self.assertEqual(Reminder.objects.get().date,
+                         datetime(2019, 4, 20, 21, 0, 0, tzinfo=pytz.UTC))
